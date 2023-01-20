fc6d3a10728b7dc4d36062f5f031ddf9ca0fe283
David Brownman <beamneocube@gmail.com>
2023-01-19 23:59:23 -0800

    wip initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# json-request
+
+Designed as the simplest, fewest-featured JSON-only HTTP request client possible.
---
+++ src/index.ts
@@ -0,0 +1,67 @@
+// can remove when fetch is non-experimental
+// maybe Node 20?
+import fetch, { type RequestInit, Headers } from 'node-fetch'
+
+const jsonMime = 'application/json'
+
+type METHOD = 'GET' | 'POST' | 'PATCH'
+
+interface Options {
+  headers?: { [k: string]: string }
+  query?: { [k: string]: string | number }
+}
+
+const sendRequest = async <Response>(
+  method: METHOD,
+  url: string,
+  // this isn't technically true, but it's fine for our purposes
+  // see https://github.com/sindresorhus/type-fest/blob/1c293f1cd2c1b2e9f95de5001e0c29544a8033b9/source/basic.d.ts#L15-L45
+  // and https://github.com/Microsoft/TypeScript/issues/1897#issuecomment-710744173
+  body?: object,
+  options?: Options
+): Promise<Response> => {
+  const headers = new Headers({ ...options?.headers })
+  // Prep
+
+  // JSON only!
+  headers.set('content-type', jsonMime)
+  headers.set('accept', jsonMime)
+
+  if (options?.query) {
+    // TODO
+  }
+
+  const payload: RequestInit = {
+    method,
+    headers,
+  }
+
+  if (body) {
+    payload.body = JSON.stringify(body)
+  }
+
+  // send request
+  const response = await fetch(url, payload)
+  const data = await response.json()
+
+  if (response.status >= 400) {
+    throw new Error(
+      `Got a ${response.status} response with body ${JSON.stringify(data)}`
+    )
+  }
+  return data as Response
+}
+
+export const get = async <Response extends object>(
+  url: string,
+  options?: Options
+): Promise<Response> => sendRequest('GET', url, undefined, options)
+
+export const post = async <Response extends object>(
+  url: string,
+  body?: object,
+  options?: Options
+): Promise<Response> => sendRequest('POST', url, body, options)
+
+// const j = await get(url)
+// const pj = await post(url, body, {headers, query})
