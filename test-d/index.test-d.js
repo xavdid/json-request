dc9b4498cbe309b0c74f840b484eeece69e201c9
David Brownman <beamneocube@gmail.com>
2023-01-23 22:48:30 -0800

    delete extra files

--- test-d/index.test-d.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const tsd_1 = require("tsd");
-const index_1 = require("../src/index");
-const response = await (0, index_1.get)('asdf');
-(0, tsd_1.expectType)(response);
